0d858bc19d7528a483cce033e8e3598dbad1e09f
Dean Cain <b01787017@studentmail.uws.ac.uk>
2026-03-19 10:50:36 +0000

    Refactor LendingPeriods to remove hard-coded constants and allow runtime updates

--- com/uws/solid/excercise/LendingPeriods.java
+++ com/uws/solid/excercise/LendingPeriods.java
@@ -3,7 +3,20 @@ package com.uws.solid.excercise;
 public final class LendingPeriods {
     private LendingPeriods() {}
 
-    public static final int STUDENT_DAYS = 14;
-    public static final int FACULTY_DAYS = 30;
-    public static final int REGULAR_DAYS = 7;
+    public static int STUDENT_DAYS = 14;
+    public static int FACULTY_DAYS = 30;
+    public static int REGULAR_DAYS = 7;
+
+    public static void updateStudentDays(int days){
+        STUDENT_DAYS = days;
+    }
+
+    public static void updateFacultyDays(int days){
+        FACULTY_DAYS = days;
+    }
+
+    public static void updateRegularDays(int days){
+        REGULAR_DAYS = days;
+    }
+
 }
